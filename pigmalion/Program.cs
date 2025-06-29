7ff2e1c84b8f74e1854c4c40eebbdb44cb6f47c8
Martín Höxter <martinhoxter@hotmail.com>
2025-06-29 04:40:49 -0300

    Creado el sistem para input dinamico de usuario.

--- pigmalion/Program.cs
+++ pigmalion/Program.cs
@@ -11,6 +11,14 @@
             Console.WriteLine();
             Console.WriteLine(Method2(array1, 8));
             Console.WriteLine(Method2(array2, 8));
+            Console.WriteLine();
+
+            int[] arrayUser = ArrayGenerator();//Pregunta tamaño y valores de un array generado por el usuario, si se ingresa 0 devuelve nulo
+            if (arrayUser != null)
+            {
+                int objective = NumberValidator();//Revisa que el numero objetivo sea valido y no sea 0 o negativo
+                Console.WriteLine(Method2(arrayUser, objective));
+            }
         }
         private static bool Method1(int[] array, int objective)
         {
@@ -39,6 +47,95 @@
                 hash.Add(num);
             }
             return false;
+        }
+        private static int[]? ArrayGenerator()
+        {
+            bool isSizeValid = false;
+            int arraySize = 0;
+            do 
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese el tamaño de array. 0 o negativos para salir de la aplicacion.");
+                    arraySize = Convert.ToInt32(Console.ReadLine());
+                    if(arraySize > 0)
+                    {
+                        isSizeValid = true;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Valor invalido.");
+                    isSizeValid = false;
+                }
+            } while (isSizeValid == false);
+            int[] array = new int[arraySize];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int value = 0;
+                bool isValueValid = false;
+                do 
+                {
+                    try
+                    {
+                        Console.WriteLine($"Ingrese el valor de la posicion {i+1} de {arraySize}.");
+                        value = Convert.ToInt32(Console.ReadLine());
+                        if(value > 0)
+                        {
+                            array[i] = value;
+                            isValueValid = true;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Valor invalido.");
+                            isValueValid = false;
+                        }
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Valor invalido.");
+                        isValueValid = false;
+                    }
+                } while (isValueValid == false);
+            }
+            return array;
+        }
+        private static int NumberValidator()
+        {
+            Console.Clear();
+            int objective = 0;
+            bool isObjectiveValid = false;
+            do 
+            {
+                Console.WriteLine("Ingrese el numero objetivo de la suma.");
+                try 
+                {
+                    objective = Convert.ToInt32(Console.ReadLine());
+                    if (objective > 0)
+                    {
+                        isObjectiveValid = true;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Valor no valido.");
+                        isObjectiveValid = false;
+                    }
+                } catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Valor no valido.");
+                    isObjectiveValid = false;
+                }
+            } while (isObjectiveValid == false);
+            return objective;
         }
     }
 }
